3183154e89dc03d8df93a5eab052fd36afad5805
eodyd <eodyd7072@naver.com>
2021-12-27 15:36:35 +0900

    Initial commit

---
+++ src/main/java/org/example/springboard/BoardController.java
@@ -0,0 +1,13 @@
+package org.example.springboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+    @RequestMapping("/list")
+    public String list() {
+        return "list";
+    }
+}
